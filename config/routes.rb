78a96c730dc1fb882a1bc3b33c27e182494a88e7
Nik Daftary <nikdaftary@gmail.com>
2013-09-24 16:36:43 -0500

    finished partner exc

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Metube::Application.routes.draw do
-  #       ACTUAL URL               VIEWS FOLDER  #Method Name
-  get "/videos/gladiator"       => "videos#show_gladiator", as: 'gladiator_video'
-  get "/videos/air_force_one"   => 'videos#show_air_force_one'
-  get '/videos/fight_club'      => 'videos#show_fight_club'
-  get '/videos/dumb_and_dumber' => 'videos#show_dumb_and_dumber'
-  get '/videos/all'             => 'videos#show_all'
+  root "videos#show_all"  #look at line 9. This refers to the Views/Folder not the as:
+
+  #       ACTUAL URL               VIEWS FOLDER                           #Named Route (/pages/week-4/rails-intro-routes)
+  get "/videos/gladiator"       => "videos#show_gladiator",       as: 'gladiator_video'
+  get "/videos/air_force_one"   => 'videos#show_air_force_one',   as: 'air_force_one_sucks'
+  get '/videos/fight_club'      => 'videos#show_fight_club',      as: 'fight_club_video'
+  get '/videos/dumb_and_dumber' => 'videos#show_dumb_and_dumber', as: 'dumb_and_dumber_video'
+  get '/videos/all'             => 'videos#show_all',             as: 'show_all_video'
 
   get 'posts/atlanta'           => 'posts#atlanta'
   get 'posts/minneapolis'       => 'posts#minneapolis'
