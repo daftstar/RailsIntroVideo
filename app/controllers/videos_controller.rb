c4a0e77e5d333b058114ba1e76edeea78584461a
Nik Daftary <nikdaftary@gmail.com>
2013-09-24 11:22:28 -0500

    completes partner challenge four; replaces hard coded values with instance variables in air force one and dumb and dumber

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,13 +3,22 @@ class VideosController < ApplicationController
 		# because of the name of the method
 		# this method automatically send the 
 		# user to a page with the name 'show_gladiatior'
-		@title = "Are you not entertained?"
-		@description = "I am so not entertained."
+		# @title = "Are you not entertained?"
+		# @description = "I am so not entertained."
 		#without defining the instance variable, the page will still render, but will not show the intented variable's value. 
-
+		@video = {
+			:title => "Gladiator",
+			:description => "This is when Russell Crowe shows he is the man",
+			:youtube_id => "FI1ylg4GKv8"
+		}
 	end
 
 	def show_air_force_one
+		@video_crap = {
+			:title 				=> "Air Force One",
+			:description	=> "a movie about planes and things",
+			:youtube_id		=> "dQw4w9WgXcQ"
+		}
 
 	end
 
@@ -17,6 +26,12 @@ class VideosController < ApplicationController
 	end
 
 	def show_dumb_and_dumber
+		@video = {
+			:title => "Dumb & Dumber",
+			:description => "a film of my formative youth.",
+			:youtube_id => "S4f4EsPBvTk"
+		}
+
 	end
 
 end
