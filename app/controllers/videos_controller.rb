49c7700ea948c4c84dbe526dc3adc71074ee115f
Nik Daftary <nikdaftary@gmail.com>
2013-09-24 10:52:49 -0500

    adding instance variables to replace hard-coded text

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,12 +3,14 @@ class VideosController < ApplicationController
 		# because of the name of the method
 		# this method automatically send the 
 		# user to a page with the name 'show_gladiatior'
+		@title = "Are you not entertained?"
+		@description = "I am so not entertained."
+		#without defining the instance variable, the page will still render, but will not show the intented variable's value. 
+
 	end
 
 	def show_air_force_one
-		#
-		#
-		#
+
 	end
 
 	def show_fight_club
@@ -16,5 +18,5 @@ class VideosController < ApplicationController
 
 	def show_dumb_and_dumber
 	end
-	
+
 end
